11fea58e5b5d2cf519c3d1e346c15e84ba119fde
Andriy <andrijs373@gmail.com>
2026-02-12 21:19:42 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 import { getAll, get5First, getRedGoods } from './api/goods';
@@ -7,17 +7,32 @@ import { Good } from './types/Good';
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
 
-  const handleLoadAll = () => {
-    getAll().then(setGoods);
-  };
+  const handleLoadAll = useCallback(() => {
+    getAll()
+      .then(setGoods)
+      .catch(() => {
+        // eslint-disable-next-line no-console
+        console.error('Failed to load all goods');
+      });
+  }, []);
 
-  const handleLoad5First = () => {
-    get5First().then(setGoods);
-  };
+  const handleLoad5First = useCallback(() => {
+    get5First()
+      .then(setGoods)
+      .catch(() => {
+        // eslint-disable-next-line no-console
+        console.error('Failed to load first 5 goods');
+      });
+  }, []);
 
-  const handleLoadRed = () => {
-    getRedGoods().then(setGoods);
-  };
+  const handleLoadRed = useCallback(() => {
+    getRedGoods()
+      .then(setGoods)
+      .catch(() => {
+        // eslint-disable-next-line no-console
+        console.error('Failed to load red goods');
+      });
+  }, []);
 
   return (
     <div className="App">
@@ -27,7 +42,11 @@ export const App: React.FC = () => {
         Load all goods
       </button>
 
-      <button type="button" data-cy="first-five-button" onClick={handleLoad5First}>
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleLoad5First}
+      >
         Load 5 first goods
       </button>
 
